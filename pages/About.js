1d83725d9d69f4235d39227ce5ea68585ac8d953
Patrick_B <pb5191@gmail.com>
2021-04-11 18:51:30 -0400

    added Test and About components

---
+++ pages/About.js
@@ -0,0 +1,32 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Alcatra incididunt biltong, esse rump chuck quis proident lorem venison.
+        Ham pariatur reprehenderit rump, bacon prosciutto sunt nulla anim.
+        Sirloin nisi swine ham, adipisicing ut duis mollit capicola sint. Ea
+        dolor in est.","Turkey biltong duis andouille. Buffalo jowl short loin
+        spare ribs alcatra consequat sausage pancetta. Exercitation consectetur
+        enim aliquip rump shankle, turducken kevin hamburger deserunt tongue
+        magna veniam commodo ham. Burgdoggen adipisicing ut, cupidatat dolore
+        ribeye duis in pork belly id irure anim prosciutto. Cupim ullamco ea,
+        officia chuck andouille ex tail biltong duis hamburger. Flank drumstick
+        ut cillum qui, bacon jerky.
+      </p>
+      <p>
+        Consectetur bacon venison ut, landjaeger incididunt ball tip esse
+        drumstick doner dolore kielbasa.","Aliqua nisi jowl officia kielbasa
+        occaecat meatloaf rump nulla fatback minim picanha labore. Dolore esse
+        consectetur, salami sunt meatball chislic anim andouille dolore picanha.
+        Pork belly exercitation burgdoggen meatloaf occaecat. Ad shoulder
+        pastrami sed biltong flank veniam, sirloin dolore andouille fatback.
+        Quis nisi in buffalo voluptate proident, andouille tri-tip sed sirloin
+        boudin elit pastrami venison.","Ribeye tri-tip bresaola enim capicola
+        do.
+      </p>
+    </div>
+  );
+};
+
+export default About;
