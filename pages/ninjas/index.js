9bc2a4709ad4cb73df7ccb39731b69b4e6fdb0d6
Patrick_B <pb5191@gmail.com>
2021-04-11 22:42:27 -0400

    added dynamic Details component

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,6 @@
 import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -9,16 +11,22 @@ export const getStaticProps = async () => {
 
 const Ninjas = ({ ninjas }) => {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-      {ninjas.map((ninja) => (
-        <div key={ninja.id}>
-          <a className={styles.single}>
-            <h3>{ninja.name}</h3>
-          </a>
-        </div>
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Listing</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
