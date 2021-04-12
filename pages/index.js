20a8da45320a61cc1a125d9a9bff05fe81abbb94
Patrick_B <pb5191@gmail.com>
2021-04-11 21:40:37 -0400

    added metadata and tab title

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,31 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Cillum nisi fatback aute sirloin, adipisicing nostrud meatloaf shankle
-        cupim veniam t-bone. Drumstick culpa in commodo, corned beef capicola
-        kevin venison chuck burgdoggen mollit deserunt cow. Ea rump do pork loin
-        anim pork drumstick ribeye ad non frankfurter quis tongue eiusmod
-        consectetur.
-      </p>
-      <p className={styles.text}>
-        Cillum nisi fatback aute sirloin, adipisicing nostrud meatloaf shankle
-        cupim veniam t-bone. Drumstick culpa in commodo, corned beef capicola
-        kevin venison chuck burgdoggen mollit deserunt cow. Ea rump do pork loin
-        anim pork drumstick ribeye ad non frankfurter quis tongue eiusmod
-        consectetur.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Cillum nisi fatback aute sirloin, adipisicing nostrud meatloaf shankle
+          cupim veniam t-bone. Drumstick culpa in commodo, corned beef capicola
+          kevin venison chuck burgdoggen mollit deserunt cow. Ea rump do pork
+          loin anim pork drumstick ribeye ad non frankfurter quis tongue eiusmod
+          consectetur.
+        </p>
+        <p className={styles.text}>
+          Cillum nisi fatback aute sirloin, adipisicing nostrud meatloaf shankle
+          cupim veniam t-bone. Drumstick culpa in commodo, corned beef capicola
+          kevin venison chuck burgdoggen mollit deserunt cow. Ea rump do pork
+          loin anim pork drumstick ribeye ad non frankfurter quis tongue eiusmod
+          consectetur.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
